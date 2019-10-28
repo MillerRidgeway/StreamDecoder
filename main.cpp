7ff2796652ffb6c58bc47f6117ee5fbf33572d13
Miller Ridgeway <miller.ridgeway@gmail.com>
2019-10-28 11:39:05 -0600

    Better tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "pkt_decoder.h"
 #include <stdio.h>
+#include <vector>
 
 static void pkt_printer(void *ctx, size_t len, const uint8_t *data) {
     (void) ctx;
@@ -11,25 +12,65 @@ static void pkt_printer(void *ctx, size_t len, const uint8_t *data) {
 }
 
 int main() {
-    const uint8_t pkt1[] = {0x02, 0xFF, 0x03, 0x03, 0x02, 0x03, 0x05, 0x03};
-    const uint8_t pkt2[] = {0x02, 0xFF, 0x10, 0x22, 0x45, 0x03};
-    const uint8_t pkt3[] = {0x02, 0x07, 0x03, 0x10};
-    const uint8_t pkt4[] = {0x02, 0x10, 0x22, 0x03};
-    const uint8_t pkt5[] = {0x02, 0x10, 0x23, 0x03};
-    const uint8_t pkt6[] = {0x02, 0x10, 0x30, 0x03};
-    const uint8_t pkt7[] = {0x02, 0x10, 0x30};
-    const uint8_t pkt8[] = {0x08, 0x03};
+    //Testing vectors
+    std::vector<const uint8_t*> packets;
+    std::vector<int> sizes;
 
+    //Test cases
+    const uint8_t pkt0[] = {}; // Blank
+    packets.push_back(pkt0);
+    sizes.push_back(sizeof(pkt0));
+
+    const uint8_t pkt1[] = {0x02, 0xFF, 0x03, 0x03, 0x02, 0x03, 0x05, 0x03}; // ff
+    packets.push_back(pkt1);
+    sizes.push_back(sizeof(pkt1));
+
+    const uint8_t pkt2[] = {0x02, 0xFF, 0x10, 0x22, 0x45, 0x03}; // ff 02 45
+    packets.push_back(pkt2);
+    sizes.push_back(sizeof(pkt2));
+
+    const uint8_t pkt3[] = {0x02, 0x07, 0x03, 0x10}; // 07 
+    packets.push_back(pkt3);
+    sizes.push_back(sizeof(pkt3));
+
+    const uint8_t pkt4[] = {0x02, 0x10, 0x22, 0x03}; //02
+    packets.push_back(pkt4);
+    sizes.push_back(sizeof(pkt4));
+
+    const uint8_t pkt5[] = {0x02, 0x10, 0x23, 0x03}; // 03
+    packets.push_back(pkt5);
+    sizes.push_back(sizeof(pkt5));
+
+    const uint8_t pkt6[] = {0x02, 0x10, 0x30, 0x03};// 10
+    packets.push_back(pkt6);
+    sizes.push_back(sizeof(pkt6));
+
+    const uint8_t pkt7[] = {0x02, 0x10, 0x30}; // Partial
+    packets.push_back(pkt7);
+    sizes.push_back(sizeof(pkt7));
+
+    const uint8_t pkt8[] = {0x08, 0x03}; // 10 08
+    packets.push_back(pkt8);
+    sizes.push_back(sizeof(pkt8));
+
+    const uint8_t pkt9[] = {0x02, 0x03}; // Blank
+    packets.push_back(pkt9);
+    sizes.push_back(sizeof(pkt9));
+
+    uint8_t pkt10[520] = {}; // Packet > MAX_PACKET_LENGTH
+    for(int i = 0; i < 520; i++){
+        pkt10[i] = 0x01;
+    }
+    packets.push_back(pkt10);
+    sizes.push_back(sizeof(pkt10));
+
+    //Write test bytes
     pkt_decoder_t *decoder = pkt_decoder_create(pkt_printer, nullptr);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt1), pkt1);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt2), pkt2);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt3), pkt3);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt4), pkt4);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt5), pkt5);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt6), pkt6);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt7), pkt7);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt8), pkt8);
+    for(int i = 0; i < packets.size(); i++){
+         pkt_decoder_write_bytes(decoder, sizes.at(i), packets.at(i));
+    }
 
+    //Cleanup
     pkt_decoder_destroy(decoder);
     return 0;
 }
