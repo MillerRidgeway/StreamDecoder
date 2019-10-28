f63911808ec515ed312af08fce0ad3c129ebf0e3
Miller Ridgeway <miller.ridgeway@gmail.com>
2019-10-27 21:48:47 -0600

    Trying to dereference the vector correctly

--- pkt_decoder.cpp
+++ pkt_decoder.cpp
@@ -14,6 +14,11 @@ struct pkt_decoder {
     pkt_read_fn_t callbackFunction;
     void *callbackContext;
 
+    vector <uint8_t> immutableDataCopy, parsedStream;
+    bool isComplete = false;
+    bool escapeFlag = false;
+    bool validStart = false;
+
     ~pkt_decoder() {}
 };
 
@@ -44,37 +49,33 @@ void pkt_decoder_write_bytes(pkt_decoder_t *decoder, size_t len, const uint8_t *
     }
 
     //Everything OK. Decode the stream.
-    vector <uint8_t> immutableDataCopy, parsedStream;
-    bool isComplete = false;
-    bool escapeFlag = false;
-    bool validStart = false;
 
     for (int i = 0; i < len; i++) {
-        immutableDataCopy.push_back(data[i]);
+        decoder->immutableDataCopy.push_back(data[i]);
 
-        if (immutableDataCopy.at(i) == 0x03) { //ETX
-            isComplete = true;
-        } else if (immutableDataCopy.at(i) == 0x02) { //STX
-            parsedStream.clear();
-            validStart = true;
-        } else if (immutableDataCopy.at(i) == 0x10) { //Found escape val
-            escapeFlag = true;
-        } else if (escapeFlag) { // Prev was escape (0x10)
-            parsedStream.push_back(immutableDataCopy.at(i) - 0x20);
-            escapeFlag = false;
-        } else if (validStart) { // Add the value if we have seen proper STX
-            parsedStream.push_back(immutableDataCopy.at(i));
+        if (decoder->immutableDataCopy.at(i) == 0x03) { //ETX
+            decoder->isComplete = true;
+        } else if (decoder->immutableDataCopy.at(i) == 0x02) { //STX
+            decoder->parsedStream.clear();
+            decoder->validStart = true;
+        } else if (decoder->immutableDataCopy.at(i) == 0x10) { //Found escape val
+            decoder->escapeFlag = true;
+        } else if (decoder->escapeFlag) { // Prev was escape (0x10)
+            decoder->parsedStream.push_back(decoder->immutableDataCopy.at(i) - 0x20);
+            decoder->escapeFlag = false;
+        } else if (decoder->validStart) { // Add the value if we have seen proper STX
+            decoder->parsedStream.push_back(decoder->immutableDataCopy.at(i));
         }
 
         //If we have found a valid packet stream print that packet and clear the parsing buffer
-        if (isComplete) {
-            if (!parsedStream.empty()) {
-                uint8_t *newData = &parsedStream[0];
-                decoder->callbackFunction(decoder->callbackContext, parsedStream.size(), newData);
+        if (decoder->isComplete) {
+            if (!decoder->parsedStream.empty()) {
+                uint8_t *newData = decoder->parsedStream.data();
+                decoder->callbackFunction(decoder->callbackContext, decoder->parsedStream.size(), newData);
             }
-            parsedStream.clear();
-            isComplete = false;
-            validStart = false;
+            decoder->parsedStream.clear();
+            decoder->isComplete = false;
+            decoder->validStart = false;
         }
     }
 
