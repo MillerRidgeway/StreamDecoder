593bcf7f2cd0c75527435921f308c5438a0167ca
Miller Ridgeway <miller.ridgeway@gmail.com>
2019-10-28 11:08:40 -0600

    Added bin

---
+++ bin/CMakeFiles/StreamDecoder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StreamDecoder.dir/main.cpp.o"
+  "CMakeFiles/StreamDecoder.dir/pkt_decoder.cpp.o"
+  "StreamDecoder.pdb"
+  "StreamDecoder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StreamDecoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
